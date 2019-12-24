c9014dc532562177f84d8e7ca57c63de650b3612
benalpha1105@gmail.com <benalpha1105@gmail.com>
2019-12-24 13:24:19 +0700

    Sua xong duong dan

--- UtThienWeb/Controllers/CunghoclambanhController.cs
+++ UtThienWeb/Controllers/CunghoclambanhController.cs
@@ -32,35 +32,6 @@ namespace UtThienWeb.Controllers
             ViewBag.topViews = db.News.OrderByDescending(a=>a.NewsViews).Take(6);
             return View(db.News.Where(a=>a.NewsTypeId==1));
         }
-        public ActionResult Details(string name)
-        {
-            HttpCookie cookie = Request.Cookies["cookieCHLB"];
-
-            if (cookie != null)
-            {
-                Session["current_user"] = db.Accounts.Find(int.Parse(cookie["id"]));
-            }
-            if (Session["current_user"] == null)
-            {
-                Session["user"] = "<a href='' class='cart-btn' data-target='#login' data-toggle='modal'><i class='fa fa-user'></i><span>Đăng nhập</span></a>";
-            }
-            else
-            {
-                Session["user"] = "<a href='#'>Chào ";
-                Session["user"] += ((UtThienWeb.Models.Account)Session["current_user"]).AccountName;
-                Session["user"] += " </a>";
-                Session["user"] += "<a href='/Accounts/Logout' class='cart-btn'><i class='fas fa-sign-out-alt'></i><span>Đăng xuất</span></a>";
-            }
-            var list = db.News.Where(a=>a.NewsTypeId==1);
-            var id = 0;
-            foreach (var item in list)
-            {
-                if (HomeController.RemoveUnicode(item.NewsTitle).Equals(name))
-                {
-                    id = item.NewsId;
-                }
-            }
-            return View(db.News.Find(id));
-        }
+        
     }
 }
